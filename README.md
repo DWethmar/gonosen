8098473d2f97e9a293337ef2c70aaa022a6dc6af
Dennis Wethmar <denniswethmar@gmail.com>
2023-01-02 11:12:49 +0100

    Setup sdl2 and gl

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # gonosen
+
+# Setup
+
+## Mac OS X
+
+- install cmake: `brew install cmake`
+- install sdl2: `brew install sdl2`
--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,37 @@
-use rand::Rng;
-use std::cmp::Ordering;
-use std::io;
+extern crate gl;
+extern crate sdl2;
 
 fn main() {
-    println!("Guess the number!");
+    let sdl = sdl2::init().unwrap();
+    let video_subsystem = sdl.video().unwrap();
+    let window = video_subsystem
+        .window("Game", 900, 700)
+        .opengl()
+        .resizable()
+        .build()
+        .unwrap();
 
-    let secret_number = rand::thread_rng().gen_range(1..=100);
+    let _gl_context = window.gl_create_context().unwrap();
+    let _gl =
+        gl::load_with(|s| video_subsystem.gl_get_proc_address(s) as *const std::os::raw::c_void);
 
-    loop {
-        println!("Please input your guess.");
+    unsafe {
+        gl::ClearColor(0.3, 0.3, 0.5, 1.0);
+    }
 
-        let mut guess = String::new();
-
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read line");
-
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
-        };
-
-        println!("You guessed: {guess}");
-
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => {
-                println!("You win!");
-                break;
+    let mut event_pump = sdl.event_pump().unwrap();
+    'main: loop {
+        for event in event_pump.poll_iter() {
+            match event {
+                sdl2::event::Event::Quit { .. } => break 'main,
+                _ => {}
             }
         }
+
+        unsafe {
+            gl::Clear(gl::COLOR_BUFFER_BIT);
+        }
+
+        window.gl_swap_window();
     }
 }
